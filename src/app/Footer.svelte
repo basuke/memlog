2db1c3ea96d83c5eb25d9676557dea107eb16ada
Basuke Suzuki <basuke.suzuki@sony.com>
2021-11-08 11:19:58 -0800

    Added play/stop feature.

--- src/app/Footer.svelte
+++ src/app/Footer.svelte
@@ -6,10 +6,33 @@ export let index = 0;
 export let memlog;
 export let config;
 
+let playing = false;
+
+function step() {
+    if (playing && index < memlog.length - 1) {
+        index += 1;
+        setTimeout(step, 17);
+    } else {
+        playing = false;
+    }
+}
+
+function start() {
+    playing = true;
+    step();
+}
+
 </script>
 
 <div class="base">
-    <input type="range" min="0" max={memlog.length - 1} bind:value={index}>
+    <div class=bar>
+        {#if playing}
+        <button on:click={ev => playing = false }><img src="/assets/stop.svg" alt="stop playing"></button>
+        {:else}
+        <button on:click={ev => start()}><img src="/assets/play.svg" alt="play"></button>
+        {/if}
+        <input type="range" min="0" max={memlog.length - 1} bind:value={index}>
+    </div>
     <table>
         {#each Object.keys(config.layers) as name}
             <tr><th>{name}</th><td><Legend types={config.layers[name].types}/></td></tr>
@@ -19,13 +42,22 @@ export let config;
 
 <style>
 
+.bar {
+    display: flex;
+}
+
 input[type=range] {
     display: block;
     width: 100%;
+    margin: 0 0.5rem;
 }
 
 th {
     text-align: right;
+}
+
+img {
+    width: 1rem;
 }
 
 </style>
